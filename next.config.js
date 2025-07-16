17040213e5a11a2a3c9fef9ceabd3fa81e19cbff
kongri61 <de0841@gmail.com>
2025-07-16 15:13:59 +0900

    Fix: ESLint 빌드 무시 설정 수정

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 // next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // ✅ Vercel에서 빌드할 때 ESLint 오류 무시
+    // 빌드 중 ESLint 오류 무시
     ignoreDuringBuilds: true,
   },
 };
